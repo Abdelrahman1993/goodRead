c53f91aec18171ac114837574e0a1001f4a36e03
Mohamed Awad <awadmohamed233@gmail.com>
2019-02-20 18:36:56 +0200

    adding nav component

--- src/components/login.js
+++ src/components/login.js
@@ -5,7 +5,9 @@ export default function Login(props) {
       <div>
         <form>
           <input type="text" placeholder="enter your username" />
+          <i className="fas fa-user"></i>
           <input type="password" placeholder="enter your password" />
+          <i className="fas fa-unlock"></i>
           <button type="submit" >Login</button>
         </form>
       </div>
---
+++ src/components/nav.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export default function Nav(props) {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">Home</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Categories <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Books</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="#">Authors</a>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#"><i className="fas fa-user-circle"></i><span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">User 1</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="#"><i className="fas fa-sign-out-alt"></i></a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
